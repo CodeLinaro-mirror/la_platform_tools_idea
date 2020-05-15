aa5822786790008b3541b99d7636e24336686c97
shilpaba <shilpaba@google.com>
2020-05-15 15:34:33 -0700

    Switch default update channel to stable for 4.0 final
    Fixes: 156531001
    Change-Id: I9baa8222f3582c86d4709caa269385bac4546d4a

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
@@ -33,7 +33,7 @@ class UpdateOptions : BaseState() {
   var lastBuildChecked by string()
 
   @get:OptionTag("UPDATE_CHANNEL_TYPE")
-  var updateChannelType by string(ChannelStatus.BETA.code) // Android Studio: 4.0 Beta
+  var updateChannelType by string(ChannelStatus.RELEASE.code)
 
   @get:OptionTag("THIRD_PARTY_PLUGINS_ALLOWED")
   var isThirdPartyPluginsAllowed by property(false)
