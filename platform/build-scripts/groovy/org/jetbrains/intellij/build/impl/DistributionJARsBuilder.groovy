388add7c1a5fff1a32dc8e1c0f33e51dab7b3918
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-06-30 18:50:21 +0300

    IDEA-CR-64425: IDEA-244722,IDEA-202110: let bundled plugins (e.g. android plugin) declare custom version
    It is important that android plugin version is not lost at least for two reasons:
    1. This version (major+minor) is used by android-gradle-plugin, so the plugin can detect that it is invoked by version of IDE that won't be able to parse gradle model (see IDEA-244722)
    2. It is not clear for users how to map IDEA version to android studio version (see IDEA-202110)
    
    This change allows AndroidPlugin to specify its version as 10.4.0.203.SNAPSHOT.1234567 (where the first 3 digits are to identify android-support [studio] version)
    
    (cherry picked from commit 1c9f1f504225784ae6b38ad84eaa792b37ea8b16)
    
    Bug: 152941155
    Test: manual
    Change-Id: I35841f3a0cf41bdd69c186bf9eb29119d0274723

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
@@ -805,7 +805,7 @@ Android Studio: This attempts to read a non-existent file. */
     }
 
     def patchedPluginXmlDir = "$buildContext.paths.temp/patched-plugin-xml/$plugin.mainModule"
-    def patchedPluginXmlPath = "$patchedPluginXmlDir/META-INF/plugin.xml"
+    def patchedPluginXmlFile = new File("$patchedPluginXmlDir/META-INF/plugin.xml")
 
     buildContext.ant.copy(file: pluginXmlPath, todir: "$patchedPluginXmlDir/META-INF")
 
@@ -819,10 +819,12 @@ Android Studio: This attempts to read a non-existent file. */
                     buildContext.applicationInfo.isEAP ? CompatibleBuildRange.RESTRICTED_TO_SAME_RELEASE
                             : CompatibleBuildRange.NEWER_WITH_SAME_BASELINE
 
-    def pluginVersion = buildContext.buildNumber.endsWith(".SNAPSHOT") ? buildContext.buildNumber + ".${new Date().format('yyyyMMdd')}"
+    def defaultPluginVersion = buildContext.buildNumber.endsWith(".SNAPSHOT") ? buildContext.buildNumber + ".${new Date().format('yyyyMMdd')}"
             : buildContext.buildNumber
 
-    setPluginVersionAndSince(patchedPluginXmlPath, pluginVersion, compatibleBuildRange)
+    def pluginVersion = plugin.versionEvaluator.apply(patchedPluginXmlFile, defaultPluginVersion)
+
+    setPluginVersionAndSince(patchedPluginXmlFile, pluginVersion, compatibleBuildRange)
     layoutBuilder.patchModuleOutput(plugin.mainModule, patchedPluginXmlDir)
   }
 
@@ -1080,10 +1082,9 @@ Android Studio: This attempts to read a non-existent file. */
     new LayoutBuilder(buildContext, COMPRESS_JARS)
   }
 
-  private void setPluginVersionAndSince(String pluginXmlPath, String pluginVersion, CompatibleBuildRange compatibleBuildRange) {
+  private void setPluginVersionAndSince(File pluginXmlFile, String pluginVersion, CompatibleBuildRange compatibleBuildRange) {
     Pair<String, String> sinceUntil = getCompatiblePlatformVersionRange(compatibleBuildRange, buildContext.buildNumber)
-    def file = new File(pluginXmlPath)
-    def text = file.text
+    def text = pluginXmlFile.text
             .replaceFirst(
                     "<version>[\\d.]*</version>",
                     "<version>${pluginVersion}</version>")
@@ -1113,7 +1114,7 @@ Android Studio: This attempts to read a non-existent file. */
     if (!text.contains("<idea-version since-build")) {
       text = text.replace(anchor, "${anchor}\n  <idea-version since-build=\"${sinceUntil.first}\" until-build=\"${sinceUntil.second}\"/>")
     }
-    file.text = text
+    pluginXmlFile.text = text
   }
 
   static Pair<String, String> getCompatiblePlatformVersionRange(CompatibleBuildRange compatibleBuildRange, String buildNumber) {
