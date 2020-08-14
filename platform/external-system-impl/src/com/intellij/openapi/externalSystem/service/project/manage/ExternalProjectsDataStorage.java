3cc40399c2d02bd7e5f0d960d0394f7710e0fd1b
Yuriy Solodkyy <solodkyy@google.com>
2020-08-14 13:12:02 +0100

    Do not report data node de-serialization errors
    These are expected exceptions when upgrading between Android
    Studio versions.
    
    Bug: 158931686
    Bug: 169877512
    Bug: 239915774
    Change-Id: I9d3bd803fc73bb7486f86f2107469d40da1565e4

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataStorage.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataStorage.java
@@ -156,7 +156,7 @@ public final class ExternalProjectsDataStorage extends SimpleModificationTracker
     }
     catch (Throwable e) {
       markDirtyAllExternalProjects();
-      LOG.error(e);
+      LOG.warn("Failed to deserialize data nodes.", e);
     }
 
     mergeLocalSettings();
