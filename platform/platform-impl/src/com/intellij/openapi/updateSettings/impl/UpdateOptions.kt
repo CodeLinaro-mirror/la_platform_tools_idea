9e83454262e7d6b774f5f9dbff8bafaaf727094f
shilpaba <shilpaba@google.com>
2021-09-30 11:46:27 -0700

    Switch default update channel to BETA for Android Studio 2021.1.1
    Bug: 201684275
    Test: N/A
    Change-Id: I7ead1af54b6aa5634402846e65d6df020c0fa334
    (cherry picked from commit 02f563382b938ac3df41caae1ca2f4ead0b0f962)

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
@@ -31,7 +31,7 @@ class UpdateOptions : BaseState() {
 
   @get:OptionTag("UPDATE_CHANNEL_TYPE")
   @get:ReportValue(possibleValues = ["eap", "milestone", "beta", "release"])
-  var updateChannelType by string(ChannelStatus.EAP.code) // Android Studio: EAP as default unless building beta, RC, stable.
+  var updateChannelType by string(ChannelStatus.BETA.code) // Android Studio: 2021.1.1 Beta
 
   @get:OptionTag("THIRD_PARTY_PLUGINS_ALLOWED")
   var isThirdPartyPluginsAllowed by property(false)
