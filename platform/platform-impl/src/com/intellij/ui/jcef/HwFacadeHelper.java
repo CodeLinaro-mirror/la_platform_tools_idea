ae00dd2fb0c91f00a46f8ca6a8011651593440bf
Raluca Sauciuc <ralucas@google.com>
2021-10-12 23:07:48 +0000

    IDEA-275764 Strange rendering of progress dialog when JCEF is not available or disabled
    (cherry picked from commit 5c1ee8da7f2f7e1b235b322c644ab288faaff6e8)
    
    Change-Id: Ic2e7358dcad9cf941849fb0e1d793cf9cb18f2f9
    Fixes: 202699311
    Fixes: 202770139
    (cherry picked from commit cebd493bd985c62655ec9c7b848f306b5b7284ad)

--- platform/platform-impl/src/com/intellij/ui/jcef/HwFacadeHelper.java
+++ platform/platform-impl/src/com/intellij/ui/jcef/HwFacadeHelper.java
@@ -86,25 +86,26 @@ public class HwFacadeHelper {
   // Android Studio: remove when commit 4ed7d550e028caf9b09cd156b3228a3e6abd6b98 is merged; we need to disable all JBCef* calls
   public static HwFacadeHelper create(@NotNull JComponent target) {
     return JBCefApp.isSupported() ?
-           new HwFacadeHelper(target) :
-           // do not provoke any JBCef* class loading
-           new HwFacadeHelper(target) {
-             @Override
-             public void addNotify() {
-             }
-             @Override
-             public void show() {
-             }
-             @Override
-             public void removeNotify() {
-             }
-             @Override
-             public void hide() {
-             }
-             @Override
-             public void paint(Graphics g, Consumer<? super Graphics> targetPaint) {
-             }
-           };
+      new HwFacadeHelper(target) :
+      // do not provoke any JBCef* class loading
+      new HwFacadeHelper(target) {
+        @Override
+        public void addNotify() {
+        }
+        @Override
+        public void show() {
+        }
+        @Override
+        public void removeNotify() {
+        }
+        @Override
+        public void hide() {
+        }
+        @Override
+        public void paint(@NotNull Graphics g, @NotNull Consumer<? super Graphics> targetPaint) {
+          targetPaint.accept(g);
+        }
+      };
   }
 
   public HwFacadeHelper(JComponent target) {
@@ -235,7 +236,7 @@ public class HwFacadeHelper {
     }
   }
 
-  public void paint(Graphics g, Consumer<? super Graphics> targetPaint) {
+  public void paint(@NotNull Graphics g, @NotNull Consumer<? super Graphics> targetPaint) {
     if (isActive()) {
       Dimension size = myTarget.getSize();
       if (myBackBuffer == null || myBackBuffer.getWidth() != size.width || myBackBuffer.getHeight() != size.height) {
