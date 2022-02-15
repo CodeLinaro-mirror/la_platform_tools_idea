JetBrains/intellij-community

[Workspace Model]: IDEA-286873 Improve ClassToIntConverter performance using positive lock
GitOrigin-RevId: 532ad6fd0a2df6fc81d628943a7611e51f13b6e4

platform/workspaceModel/storage/tests/testSrc/com/intellij/workspaceModel/storage/propertyBased/VirtualFileIndexTest.kt
@@ -145,7 +145,7 @@ class VirtualFileIndexTest {
     ))
     val id = env.generate(Generator.integers(0, 100))
     
-    createEntityId(id, ClassToIntConverter.getInt(clazz))
+    createEntityId(id, ClassToIntConverter.INSTANCE.getInt(clazz))
   }
 
   internal val propertyGenerator = Generator.from { env ->

CodeLinaro-mirror/la_platform_tools_idea

(cherry picked from commit 3f5520e436a788cb67baad1b78ffefec93fb1f36)
Change-Id: If68923642c7c2334a8776d2cd099afa5219ce69d

platform/workspaceModel/storage/tests/testSrc/com/intellij/workspaceModel/storage/propertyBased/VirtualFileIndexTest.kt
@@ -144,7 +144,7 @@ class VirtualFileIndexTest {
     ))
     val id = env.generate(Generator.integers(0, 100))
     
-    EntityId(id, ClassToIntConverter.getInt(clazz))
+    EntityId(id, ClassToIntConverter.INSTANCE.getInt(clazz))
   }
 
   internal val propertyGenerator = Generator.from { env ->