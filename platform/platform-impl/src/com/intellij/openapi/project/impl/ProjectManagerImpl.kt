06bc30802fe5251abe0eb3c67b880421b4c7eee7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-23 09:17:01 +0100

    IDEA-307186 get rid of invokeAndWait
    (cherry picked from commit eba1d6186ae2c3ee4a2a6666f3f3d9519e2b9bdb)
    
    Change-Id: I1e8363ba8dd2e9cf3a03bb0f59a7cae953bfffe3

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.kt
@@ -37,7 +37,6 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.progress.ProcessCanceledException
-import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.progress.impl.CoreProgressManager
 import com.intellij.openapi.project.*
 import com.intellij.openapi.project.ex.ProjectEx
@@ -876,10 +875,8 @@ open class ProjectManagerImpl : ProjectManagerEx(), Disposable {
     if (options.runConversionBeforeOpen) {
       val conversionService = ConversionService.getInstance()
       if (conversionService != null) {
-        conversionResult = blockingContext {
-          runActivity("project conversion") {
-            conversionService.convert(projectStoreBaseDir)
-          }
+        conversionResult = runActivity("project conversion") {
+          conversionService.convert(projectStoreBaseDir)
         }
         if (conversionResult.openingIsCanceled()) {
           throw CancellationException("ConversionResult.openingIsCanceled() returned true")
