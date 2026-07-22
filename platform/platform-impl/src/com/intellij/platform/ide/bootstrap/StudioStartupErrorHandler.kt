208a09e4a33de4b22fd107a22f653d1f2d26fe6c
Vishal Gupta <vishalga@google.com>
2026-07-22 05:35:10 +0000

    Redirect DirectoryLock.CannotActivateException to b/442392353
    When the IDE fails to start due to `DirectoryLock.CannotActivateException`
    (e.g., another instance is still running and not responding), redirect
    the user to the known issue b/442392353 to vote +1, instead of directing
    them to file a new duplicate bug.
    
    We check the exception class name dynamically to avoid a compile-time
    circular dependency between `platform-impl` (where
    `StudioStartupErrorHandler` resides) and `bootstrap` (where `DirectoryLock`
    resides).
    
    Report Error Pop Up Before Fix
    https://drive.google.com/file/d/1lhPl5c0sZDL89gxbRF0yDFMcBp05JMJ0/view?usp=drive_link&resourcekey=0-QolYHwiCzU0AvmRswt8iLA
    Report Error Redirect Before Fix
    https://drive.google.com/file/d/1hnKiLRN9MFeP4MSZ1zZksRmYYxRax4AO/view?usp=drive_link&resourcekey=0-Gdxz4FqcGI88zBpLRyOSmw
    
    Report Error Pop Up After Fix
    https://drive.google.com/file/d/1ZyC4qMDSu6o10FSe7QpOevabGsnsWSyG/view?usp=drive_link&resourcekey=0-1SqYulhRH6U7A6CfotKzmw
    Report Error Redirect After Fix
    https://drive.google.com/file/d/1k5qdwKuAqSengWo8XvC2lxtILheVGejr/view?usp=drive_link&resourcekey=0-Zn1sgUe1rzA9nliiw2_9Bg
    
    Bug: 537563127
    Test: NA
    Change-Id: I293b4aabef45120c14a367c8dd92c504c8fb727a

--- platform/platform-impl/src/com/intellij/platform/ide/bootstrap/StudioStartupErrorHandler.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/bootstrap/StudioStartupErrorHandler.kt
@@ -5,12 +5,35 @@ import java.nio.file.Path
 
 class StudioStartupErrorHandler : StartupErrorHandler {
   override fun uploadLogs(error: Throwable, logs: Path?): String {
-    val url = "https://issuetracker.google.com/issues/new?component=192708"
+    val isCannotActivate = isCannotActivateException(error)
+    val url = if (isCannotActivate) {
+      "https://issuetracker.google.com/issues/442392353"
+    } else {
+      "https://issuetracker.google.com/issues/new?component=192708"
+    }
 
     // Automatically launch the default web browser to the bug ticket
     BrowserUtil.browse(url)
 
-    return "N/A\nOpened bug tracker in your browser.\n" +
-           "If it didn't open, please file this issue at: $url"
+    return if (isCannotActivate) {
+      "N/A\nOpened known issue b/442392353 in your browser.\n" +
+      "Please vote +1 on the issue if the issue persists.\n" +
+      "If it didn't open, please visit: $url"
+    } else {
+      "N/A\nOpened bug tracker in your browser.\n" +
+      "If it didn't open, please file this issue at: $url"
+    }
+  }
+
+  private fun isCannotActivateException(error: Throwable): Boolean {
+    var current: Throwable? = error
+    var depth = 0
+    while (current != null && depth++ < 1000) {
+      if (current.javaClass.name == "com.intellij.platform.ide.bootstrap.DirectoryLock\$CannotActivateException") {
+        return true
+      }
+      current = current.cause
+    }
+    return false
   }
 }
